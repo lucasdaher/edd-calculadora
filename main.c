2ae13f2954198b1283da76a924f361fe3221ebf6
lucasdaher <contato@lucasdaher.com>
2024-11-14 02:46:50 -0300

    implementando testes realizados pelo grupo.

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
+  printf("-------------------------------------------------------------\n");
+  printf("TESTES DO PROFESSOR: \n");
+  printf("-------------------------------------------------------------\n\n");
+
   Expressao expressoes[] = {
       {"3 4 + 5 *", "(3 + 4) * 5", 35},
       {"7 2 * 4 +", "7 * 2 + 4", 18},
@@ -17,8 +21,26 @@ int main()
   for (int i = 0; i < 9; i++)
   {
     float resultado = getValor(expressoes[i].posFixa);
-    printf("Expressao %s\nResultado do pai - %.2f\nGabarito do marselo - %.2f\n\n",
+    printf("Expressao %s\nResultado - %.2f\nGabarito - %.2f\n\n",
            expressoes[i].posFixa, resultado, expressoes[i].Valor);
+  }
+
+  printf("-------------------------------------------------------------\n");
+  printf("TESTES REALIZADOS PELO GRUPO: \n");
+  printf("-------------------------------------------------------------\n\n");
+
+  Expressao testesDoGrupo[] = {
+      {"6 3 * 4 +", "6 * 3 + 4", 22},
+      {"10 3 4 * + 5 -", "10 + 3 * 4 - 5", 17},
+      {"8 5 6 2 * + +", "8 + 5 + 6 * 2", 25},
+      {"3 log 2 + 10 *", "(log 3 + 2) * 10", 24.77},
+      {"45 2 + 60 cos *", "(45 + 2) * cos(60)", 23.5}};
+
+  for (int i = 0; i < 5; i++)
+  {
+    float resultado = getValor(testesDoGrupo[i].posFixa);
+    printf("Expressao %s\nResultado - %.2f\nGabarito - %.2f\n\n",
+           testesDoGrupo[i].posFixa, resultado, testesDoGrupo[i].Valor);
   }
 
   return 0;
